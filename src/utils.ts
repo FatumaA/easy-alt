9168f2365d604d0e6b85145ae85b80b1595117bd
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-11-11 15:23:32 +0300

    refactor: move model initialisation and helper func to file

---
+++ src/utils.ts
@@ -0,0 +1,37 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import * as dotenv from "dotenv";
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
+export const initModel = async () => {
+	let captioner: any;
+	const HfInference = (await import("@huggingface/inference")).HfInference;
+
+	const hfToken = process.env.HF_TOKEN;
+
+	if (!hfToken) {
+		vscode.window.showErrorMessage(
+			"HF_TOKEN environment variable is not set. Please set it to use Easy Alt."
+		);
+		return;
+	}
+	captioner = new HfInference(hfToken);
+
+	return captioner;
+};
+
+export const downloadImage = async (url: string): Promise<Buffer> => {
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(
+				`Failed to download image: ${response.status} ${response.statusText}`
+			);
+		}
+		const arrayBuffer = await response.arrayBuffer();
+		return Buffer.from(arrayBuffer);
+	} catch (error) {
+		console.error("Error in downloadImage:", error);
+		throw error;
+	}
+};
