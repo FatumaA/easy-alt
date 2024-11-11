f0489612c0c1960d4359ee28d8f64890efe21447
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-11-11 16:51:00 +0300

    feat: set up logging with pino and highlight

---
+++ src/highlight.ts
@@ -0,0 +1,32 @@
+import * as path from "path";
+import * as dotenv from "dotenv";
+import type { LoggerOptions } from "pino";
+import pino from "pino";
+import type { NodeOptions } from "@highlight-run/node";
+
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
+const highlightConfig = {
+	projectID: process.env.HIGHLIGHT_PROJECT_ID!,
+	serviceName: "easy-alt",
+	serviceVersion: "0.0.4",
+	environment: "production",
+} as NodeOptions;
+
+const pinoConfig = {
+	level: "debug",
+	transport: {
+		target: "@highlight-run/pino",
+		options: highlightConfig,
+	},
+} as LoggerOptions;
+
+if (
+	typeof process.env.NEXT_RUNTIME === "undefined" ||
+	process.env.NEXT_RUNTIME === "nodejs"
+) {
+	const { H } = require("@highlight-run/node");
+	H.init(highlightConfig);
+}
+
+export const logger = pino(pinoConfig);
